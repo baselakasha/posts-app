97235a05415f3fa0a66abb76bef7bbcd02de166f
Basel Akasha <contact@baselakasha.com>
2023-07-11 19:44:33 +0100

    User can update details

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,4 +3,5 @@ from app import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('update-profile/<int:pk>', views.UpdateProfileView.as_view(), name='update-profile'),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,4 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
+
+from app.models import MyUser
 
 class HomePageView(TemplateView):
     template_name = 'app/index.html'
+
+class UpdateProfileView(UpdateView):
+    model = MyUser
+    template_name = 'app/profile.html'
+    success_url = "/"
+    fields = ['first_name', 'last_name', 'email']
